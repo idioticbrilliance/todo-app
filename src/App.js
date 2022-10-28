5e1c4264ea4d9dd5cc43cb08a9f12f1c9a265032
idioticbrialliance <dumbfreshdope@gmail.com>
2022-10-28 21:27:19 +0700

    todo app Complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react';
+
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
